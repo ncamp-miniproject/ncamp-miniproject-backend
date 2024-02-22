fae69dc7fd33a3c2dea9d089741e77ca56ef5d9f
pgd <rudeh1253@gmail.com>
2024-02-22 16:57:42 +0900

    write sql code for purchase database

--- src/main/webapp/sql/purchase/getpurchaselist.sql
+++ src/main/webapp/sql/purchase/getpurchaselist.sql
@@ -1,31 +1,31 @@
 SELECT
-    t.ROW_NUM "row_num",
-    t.TRAN_NO "tran_no",
-    t.BUYER_ID "buyer_id",
-    t.PAYMENT_OPTION "payment_option",
-    t.RECEIVER_NAME "receiver_name",
-    t.RECEIVER_PHONE "receiver_phone",
-    t.DEMAILADDR "demailaddr",
-    t.DLVY_REQUEST "dlvy_request",
-    t.TRAN_STATUS_CODE "tran_status_code",
-    t.ORDER_DATE "order_date",
-    t.DLVY_DATE "dlvy_date",
-    t.COUNT "count",
-    tp.prod_no "prod_no",
-    tp.quantity "quantity",
-    p.prod_name "prod_name",
-    p.prod_detail "prod_detail",
-    p.manufacture_day "manufacture_day",
-    p.price "price",
-    p.image_file "image_file",
-    p.reg_date "reg_date",
-    p.stock "stock"
+    t.row_num          "row_num",
+    t.tran_no          "tran_no",
+    t.buyer_id         "buyer_id",
+    t.payment_option   "payment_option",
+    t.receiver_name    "receiver_name",
+    t.receiver_phone   "receiver_phone",
+    t.demailaddr       "demailaddr",
+    t.dlvy_request     "dlvy_request",
+    t.tran_status_code "tran_status_code",
+    t.order_date       "order_date",
+    t.dlvy_date        "dlvy_date",
+    t.count            "count",
+    tp.prod_no         "prod_no",
+    tp.quantity        "quantity",
+    p.prod_name        "prod_name",
+    p.prod_detail      "prod_detail",
+    p.manufacture_day  "manufacture_day",
+    p.price            "price",
+    p.image_file       "image_file",
+    p.reg_date         "reg_date",
+    p.stock            "stock"
 FROM
     (
         SELECT
             ROW_NUMBER() OVER (
                 ORDER BY
-                    order_date DESC
+                    tran_no DESC
             ) ROW_NUM,
             t.tran_no TRAN_NO,
             t.buyer_id BUYER_ID,
@@ -41,11 +41,11 @@ FROM
         FROM
             transaction t
         WHERE
-            buyer_id %s
+            buyer_id = ?
         ORDER BY
-            order_date DESC
+            tran_no DESC
     ) t
     INNER JOIN transaction_prod tp ON tp.tran_no = t.tran_no
     INNER JOIN product p ON tp.prod_no = p.prod_no
-WHERE
-    v.ROW_NUM BETWEEN ?
+WHERE t.row_num between ? AND ?
+ORDER BY t.row_num
--- src/main/webapp/sql/purchase/insertpurchase.sql
+++ src/main/webapp/sql/purchase/insertpurchase.sql
@@ -1,12 +1,23 @@
-INSERT INTO transaction (tran_no, 
-                         prod_no, 
-                         buyer_id, 
-                         payment_option, 
-                         receiver_name, 
-                         receiver_phone, 
-                         demailaddr, 
-                         dlvy_request, 
-                         tran_status_code, 
-                         order_date, 
-                         dlvy_date) 
-VALUES (seq_transaction_tran_no.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+INSERT INTO transaction (
+    tran_no,
+    buyer_id,
+    payment_option,
+    receiver_name,
+    receiver_phone,
+    demailaddr,
+    dlvy_request,
+    tran_status_code,
+    order_date,
+    dlvy_date
+) VALUES (
+    seq_transaction_tran_no.NEXTVAL,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?,
+    ?
+)
